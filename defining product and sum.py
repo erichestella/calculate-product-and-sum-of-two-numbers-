6ea97fa28656127556e0afe73e7fee6f932be78b
erichestella <erichestella05@gmail.com>
2024-01-21 22:28:40 +0800

    defines the numbers input

--- defining product and sum.py
+++ defining product and sum.py
@@ -1,7 +1,13 @@
 #title of the program
 multiply_and_add = print('TYPE THE GIVEN 1 NUMBERS:\n')
 
+#defining the numbers in 
+first = 20 
+second = 30
+
 # given 1 numbers 
 first_number = input('GIVEN 1 INTEGER : \n')
 second_number = input('GIVEN 1 INTEGER : \n')
+
+total_product = first_number * second_number 
 
