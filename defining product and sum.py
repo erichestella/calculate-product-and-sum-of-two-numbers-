418907c32a50fd6536ece7a58c30a941e2fd1812
erichestella <erichestella05@gmail.com>
2024-01-21 22:13:55 +0800

    given 1 numbers

--- defining product and sum.py
+++ defining product and sum.py
@@ -1,4 +1,7 @@
 #title of the program
 multiply_and_add = print('TYPE THE GIVEN 1 NUMBERS:\n')
 
+# given 1 numbers 
+first_number = input('GIVEN 1 INTEGER : \n')
+second_number = input('GIVEN 1 INTEGER : \n')
 
